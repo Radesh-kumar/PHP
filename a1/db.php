da3e1ca91bab866865763774bfe7d450c1cd8bb9
Radesh-kumar <imradesh@gmail.com>
2020-07-12 22:05:43 +0530

    1st commit

---
+++ a1/db.php
@@ -0,0 +1,7 @@
+<?php
+  $server="us-cdbr-east-02.cleardb.com";
+  $username="b7f874d5e366ad";
+  $password="f2f44323";
+  $db="heroku_a5ff6984e7bd9fd";
+  $conn=mysqli_connect($server,$username,$password,$db);
+ ?>
